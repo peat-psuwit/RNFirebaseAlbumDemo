979fbe74963e6930ed2434e8a18f1b827ff27ec6
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-11-07 03:35:19 +0700

    Init screens skeleton

---
+++ screens/AlbumDetailScreen/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+class AlbumDetailScreen extends React.Component {
+  static navigationOptions = {
+    title: 'AlbumDetailScreen',
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>AlbumDetailScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumDetailScreen;
---
+++ screens/AlbumListScreen/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+class AlbumListScreen extends React.Component {
+  static navigationOptions = {
+    title: 'AlbumListScreen',
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>AlbumListScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumListScreen;
